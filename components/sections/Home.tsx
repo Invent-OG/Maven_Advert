b8f110e7b338c2a71e054244b4b8882b6c7486bd
rahulachuz <rahulachuz69@gmail.com>
2025-07-29 13:56:15 +0530

    spline added

--- components/sections/Home.tsx
+++ components/sections/Home.tsx
@@ -1,30 +1,11 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
-
-const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false });
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
-  const [showSpline, setShowSpline] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSpline(true);
-    }, 1000); // delay by 1s AFTER page has loaded
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <main className='h-[100vh]'>
-      {showSpline ? (
-        <Spline scene="https://prod.spline.design/HWnmevWuj3p1HU0Y/scene.splinecode" />
-      ) : (
-        <div className='flex items-center justify-center h-full bg-black text-white'>
-          Loading 3D...
-        </div>
-      )}
+      <Spline
+        scene="https://prod.spline.design/HWnmevWuj3p1HU0Y/scene.splinecode" 
+      />
     </main>
   );
 }
