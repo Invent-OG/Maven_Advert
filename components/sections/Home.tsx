f13aed68a29c8680a00d0f065a192e94b6126c14
rahulachuz <rahulachuz69@gmail.com>
2025-07-29 14:29:10 +0530

    spline removed

--- components/sections/Home.tsx
+++ components/sections/Home.tsx
@@ -2,10 +2,11 @@ import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
-    <main className='h-[100vh]'>
-      <Spline
+    <main className='h-[100vh] text-white text-9xl font-bold text-center justify-center flex items-center'>
+      {/* <Spline
         scene="https://prod.spline.design/HWnmevWuj3p1HU0Y/scene.splinecode" 
-      />
+      /> */}
+        Maven Advert
     </main>
   );
 }
