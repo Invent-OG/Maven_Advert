42d9326e35fd1302d720296034273336b764fd1d
rahulachuz <rahulachuz69@gmail.com>
2025-07-30 15:23:13 +0530

    gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,10 +1,18 @@
+import Gallery from '@/components/sections/gallerypage/Gallery'
+import GridShowcase from '@/components/sections/gallerypage/GridShowcase'
+import HeroGallery from '@/components/sections/gallerypage/HeroGallery'
+import LetsTalkGallery from '@/components/sections/gallerypage/LetsTalkGallery'
+import HeroImages from '@/components/sections/HeroImages'
 import React from 'react'
 
 function page() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeroGallery/>
+      <LetsTalkGallery/>
+      {/* <Gallery/> */}
+      <GridShowcase/>
+    </>
   )
 }
 
